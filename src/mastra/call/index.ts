d8ed07467a4590a38be855c3bc0f561eaf26554f
Lambe Boluwatife <boluwatifelambe@gmail.com>
2025-06-24 16:13:03 +0100

    feat: add password reset functionality with confirmation email on success using google send mail tool from arcade

---
+++ src/mastra/call/index.ts
@@ -0,0 +1,81 @@
+import Arcade from "@arcadeai/arcadejs";
+
+const USER_ID = process.env.USER_ID;
+
+export const sendMail = async ({ toolInput }) => {
+  const client = new Arcade();
+
+  const auth = await client.tools.authorize({
+    tool_name: "Google.SendEmail@1.2.1",
+    user_id: USER_ID,
+  });
+
+  if (auth?.status !== "completed") {
+    console.log(`Click here to authorize the tool: ${auth?.url}`);
+  }
+
+  const { status } = await client.auth.waitForCompletion(auth);
+
+  if (status !== "completed") {
+    throw new Error("Authorization failed");
+  }
+
+  console.log("🚀 Authorization successful!");
+
+  const result = await client.tools.execute({
+    tool_name: "Google.SendEmail@1.2.1",
+    input: {
+      starred: "true",
+      subject: toolInput.subject,
+      body: toolInput.body,
+      recipient: toolInput.recipient,
+    },
+    user_id: USER_ID,
+  });
+
+  return result;
+};
+
+export const passwordResetCall = async (email: string, newPassword: string) => {
+  const url = 'https://brightlife-enhancement.onrender.com/api/admin/reset-password';
+  const body = {
+    email,
+    newPassword,
+  };
+  
+  try {
+    const response = await fetch(url, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(body),
+    });
+    
+    const data = await response.json();
+    
+    if (response.ok) {
+      return {
+        success: true,
+        message: data.message,
+      };
+    }
+    
+    return {
+      success: false,
+      error: data.message || data.error || `Server returned ${response.status}: ${response.statusText}`
+    };
+  } catch (error) {
+    console.error('Error registering user:', {
+      error: error instanceof Error ? error.message : 'Unknown error',
+      url,
+    });
+
+    console.log("error:", error)
+    
+    return {
+      success: false,
+      error: error instanceof Error ? error.message : 'Unknown error',
+    };
+  }
+};
---
+++ src/mastra/index.ts
@@ -0,0 +1,19 @@
+
+import { Mastra } from '@mastra/core/mastra';
+import { PinoLogger } from '@mastra/loggers';
+import { LibSQLStore } from '@mastra/libsql';
+import { weatherWorkflow } from './workflows/weather-workflow';
+import { resetPasswordAgent } from './agents';
+
+export const mastra = new Mastra({
+  workflows: { weatherWorkflow },
+  agents: { resetPasswordAgent },
+  storage: new LibSQLStore({
+    // stores telemetry, evals, ... into memory storage, if it needs to persist, change to file:../mastra.db
+    url: ":memory:",
+  }),
+  logger: new PinoLogger({
+    name: 'Mastra',
+    level: 'info',
+  }),
+});
